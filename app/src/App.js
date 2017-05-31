3dbfc7b1afcfac0bb44252699e69405524466593
elwirra <rakowska.a@gmail.com>
2017-05-31 15:20:11 +0200

    added story book

--- app/src/App.js
+++ app/src/App.js
@@ -7,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>10Clouds React UI Kit</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
