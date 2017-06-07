85b6016f27e0d033dd311522d878d8f91583669c
elwirra <rakowska.a@gmail.com>
2017-06-07 10:34:52 +0200

    added styles components

--- app/src/lib/Radio.js
+++ app/src/lib/Radio.js
@@ -1,16 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 
 const radioStyles = {
-  border: '1px solid #eee',
-  borderRadius: 3,
-  backgroundColor: '#FFFFFF',
-  fontSize: 15,
-  margin: 10,
+  // &:checked,
+  // &:not(:checked) {
+  //   position: absolute;
+  //   left: -9999px;
+  // }
 };
 
+const Title = styled.h1`
+	color: pink;
+`;
+
 export const Radio = ({ children, onClick }) => (
-  <radio style={radioStyles} />
+  <div>
+  	  <Title>Radio boxes</Title>
+	  <p>
+	  	<label htmlFor="ice-cream">Ice cream</label>
+	  	<input id="ice-cream" type="radio" name="radio-group" style={radioStyles} defaultChecked />
+	  </p>
+	  <p>
+	  	<label htmlFor="cotton-candy">Cotton candy</label>
+	  	<input id="cotton-candy" type="radio" name="radio-group" style={radioStyles} />
+	  </p>
+  </div>
 );
 
 export default Radio;
