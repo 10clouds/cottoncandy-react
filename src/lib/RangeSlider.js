eec4c442fc67445898aac8e5babe5f5d1453bf2b
Marek Grzybek <feimosi@onet.pl>
2017-07-27 20:19:56 +0200

    Implement Range Slider

---
+++ src/lib/RangeSlider.js
@@ -0,0 +1,184 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import styled from 'styled-components';
+
+import * as palette from '../palette';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledSliderValue = styled.span`
+  display: inline-block;
+  padding: 0 16px;
+  min-width: 40px;
+`;
+
+const StyledSlider = styled.div`
+  cursor: pointer;
+  width: 100%;
+  position: relative;
+  display: block;
+  height: 4px;
+  border-radius: 6px;
+  background-color: #ebeff5;
+  outline: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const StyledSliderFill = styled.div`
+  position: absolute;
+  top: 0;
+  height: 100%;
+  background-image: linear-gradient(to bottom, #6f8aee, #4862e1);
+  border-radius: 10px;
+`;
+
+const StyledSliderHandle = styled.div`
+  top: -7px;
+  touch-action: pan-y;
+  cursor: pointer;
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  background-image: linear-gradient(to bottom, #6f8aee, #4862e1);
+  border-radius: 50%;
+`
+
+class RangeSlider extends Component {
+  static propTypes = {
+    min: PropTypes.number,
+    max: PropTypes.number,
+    step: PropTypes.number,
+    value: PropTypes.number,
+  };
+
+  static defaultProps = {
+    min: 0,
+    max: 100,
+    step: 1,
+  }
+
+  state = {
+    leftOffset: 0,
+    value: 0,
+  };
+
+  grabPosition = 8;
+
+  componentDidMount() {
+    if (typeof this.props.value === 'number') {
+      this.setBasedOnValue(this.props.value);
+    } else {
+      this.setBasedOnValue(this.props.min);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (typeof this.props.value === 'number' && this.props.value !== prevProps.value) {
+      this.setBasedOnValue(this.props.value);
+    }
+  }
+
+  onMouseDown = (e) => {
+    e.preventDefault();
+    this.grabPosition = e.clientX - (this.sliderElement.getBoundingClientRect().x + this.thumbElement.offsetLeft);
+    document.addEventListener('mousemove', this.onMouseMove)
+    document.addEventListener('mouseup', this.onMouseUp)
+  };
+
+  onMouseUp = (e) => {
+    e.preventDefault();
+    document.removeEventListener('mousemove', this.onMouseMove)
+    document.removeEventListener('mouseup', this.onMouseUp)
+  };
+
+  onMouseMove = (e) => {
+    this.setBasedOnPosition(e.clientX);
+  };
+
+  handleClick = (e) => {
+    this.setBasedOnPosition(e.clientX);
+  };
+
+  setBasedOnPosition(position) {
+    const leftOffset = this.getLeftOffset(position, this.maxPosition);
+    const value = this.getScaledValue(leftOffset, this.maxPosition)
+    const steppedLeftOffset = (this.getValue(leftOffset, this.maxPosition) * this.maxPosition) / this.props.max;
+
+    this.setState({
+      leftOffset: steppedLeftOffset,
+      value,
+    })
+  }
+
+  setBasedOnValue(value) {
+    const maxPosition = this.maxPosition;
+    const leftOffset = maxPosition * (value - this.props.min) / this.props.max;
+    const steppedLeftOffset = ((this.getValue(leftOffset, maxPosition) * maxPosition) / this.props.max);
+
+    this.setState({
+      leftOffset,
+      value,
+    });
+  }
+
+  getValue(leftOffset, maxOffset) {
+    const value = Math.round(leftOffset / maxOffset * 100);
+    const scaledValue = ((value) * (this.props.max)) / 100;
+    const steppedValue = Math.min(Math.round(scaledValue / this.props.step) * this.props.step, this.props.max);
+    return steppedValue;
+  }
+
+  getScaledValue(leftOffset, maxOffset) {
+    const value = Math.round(leftOffset / maxOffset * 100);
+    const scaledValue = (((value) * (this.props.max - this.props.min)) / 100) + this.props.min;
+    const steppedValue = Math.min(Math.round(scaledValue / this.props.step) * this.props.step, this.props.max);
+    return steppedValue;
+  }
+
+  getLeftOffset(pointerPosition, maxPosition) {
+    const newPosition = Math.round(pointerPosition - this.sliderElement.getBoundingClientRect().x - this.grabPosition);
+    const leftOffset = Math.max(0, Math.min(newPosition, maxPosition));
+    return leftOffset;
+  }
+
+  get maxPosition() {
+    const sliderElementHalfWidth = this.thumbElement.getBoundingClientRect().width / 2;
+    const maxPosition = this.sliderElement.getBoundingClientRect().width - sliderElementHalfWidth;
+    return maxPosition;
+  }
+
+  render() {
+    return (
+      <StyledContainer
+        style={ { width: `${this.props.width || 300}px` } }
+        onClick={ this.handleClick }
+      >
+        <input
+          type="range"
+          style={ { display: 'none' } }
+          value={ this.state.value }
+        />
+        <StyledSlider ref={ el => this.sliderElement = findDOMNode(el) }>
+          <StyledSliderFill style={ { width: this.state.leftOffset + 8 } } />
+          <StyledSliderHandle
+            ref={ el => this.thumbElement = findDOMNode(el) }
+            style={ { left: this.state.leftOffset } }
+            onMouseDown={ this.onMouseDown }
+          />
+        </StyledSlider>
+        <StyledSliderValue>
+         { this.state.value }
+        </StyledSliderValue>
+      </StyledContainer>
+    );
+  }
+};
+
+export default RangeSlider;
---
+++ src/stories/RangeSlider.story.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
+
+import RangeSlider from '../lib/RangeSlider';
+import { Row, StoryItem } from './utils.js';
+
+const stories = storiesOf('Core', module);
+
+stories
+  .addDecorator(withKnobs)
+  .addWithInfo('RangeSlider', '', () => (
+    <div>
+      <Row title="Basic">
+        <StoryItem title="Default">
+          <RangeSlider />
+        </StoryItem>
+        <StoryItem title="With initial value">
+          <RangeSlider value={ 50 } />
+        </StoryItem>
+        <StoryItem title="With custom step">
+          <RangeSlider step={ 20 } />
+        </StoryItem>
+      </Row>
+
+      <Row title="Min / Max">
+        <StoryItem title="With min value">
+          <RangeSlider min={ 20 } />
+        </StoryItem>
+        <StoryItem title="With max value">
+          <RangeSlider max={ 500 } />
+        </StoryItem>
+        <StoryItem title="With min & max value">
+          <RangeSlider min={ 2000 } max={ 2017 } />
+        </StoryItem>
+      </Row>
+
+      <Row title="Playground">
+        <StoryItem>
+          <RangeSlider
+            min={ number('Min', 0) }
+            max={ number('Max', 100) }
+            step={ number('step', 1) }
+            value={ number('Value', 0) }
+            width={ number('Width', 300)}
+          />
+        </StoryItem>
+      </Row>
+    </div>
+  ));
