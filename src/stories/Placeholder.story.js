e616512badfbd71898c4c62e04f6fb8eb5089226
Wiktor Latanowicz <wiktor.latanowicz@10clouds.com>
2017-09-05 12:25:55 +0200

    Remove unused imports

--- src/stories/Placeholder.story.js
+++ src/stories/Placeholder.story.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
 
 import Placeholder from '../lib/Placeholder';
--- src/stories/StepProgress.story.js
+++ src/stories/StepProgress.story.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 import StepProgress from '../lib/StepProgress';
 import { Row, StoryItem } from './utils.js';
