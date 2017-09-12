650cc9da9279ecc2687ee16e6954bb63c11bfa79
Wiktor Latanowicz <wiktor.latanowicz@10clouds.com>
2017-09-12 17:25:38 +0200

    Select input

---
+++ src/lib/Select.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { palette, style } from '../constants';
+
+const SelectWrapper = styled.div`
+  position: relative;
+  &:after {
+    border-color: ${palette.GRAY.black} transparent;
+    border-style: solid;
+    border-width: 6px 6px 0 6px;
+    content: "";
+    height: 0;
+    margin-top: -3px;
+    position: absolute;
+    right: 16px;
+    top: 50%;
+    width: 0;
+    pointer-events: none;
+  }
+`;
+
+const StyledSelect = styled.select`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  
+  background: ${style.BACKGROUND.main};
+  border: solid 1px ${(p) => p.valid ? palette.SECONDARY.base : palette.RED.base};
+  border-radius: 2px;
+  padding: 8px 12px;
+  position: relative;
+  height: 42px;
+  min-width: 226px;
+  font-size: 20px;
+  left: 0;
+  margin: 0;
+  overflow: hidden;
+
+  :focus {
+    outline: 0;
+    box-shadow: ${style.SHADOW.outline};
+  }
+  
+  :invalid {
+    color: ${palette.GRAY.base};
+  }
+`;
+
+const PlaceholderOption = styled.option`
+  color: ${palette.GRAY.base};
+  display: none;
+`;
+
+export const Option = styled.option`
+  color: ${palette.GRAY.black};
+`;
+
+
+const Select = (props) => (
+  <SelectWrapper>
+    <StyledSelect {...props} required={!!props.placeholder}>
+      {props.placeholder && (
+        <PlaceholderOption disabled hidden value="">{props.placeholder}</PlaceholderOption>
+      )}
+      {props.options && props.options.map((opt, idx) => (
+        <Option key={opt + idx} value={opt}>{opt}</Option>
+      ))}
+      {props.children}
+    </StyledSelect>
+  </SelectWrapper>
+);
+
+Select.defaultProps = {
+  valid: true,
+};
+
+Select.propTypes = {
+  placeholder: PropTypes.string,
+  children: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default Select;
---
+++ src/stories/Select.story.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+
+import Select, {Option} from '../lib/Select';
+import { Row, StoryItem } from './utils.js';
+
+const stories = storiesOf('Core', module);
+
+stories
+  .addDecorator(withKnobs)
+  .addWithInfo('Select', '', () => (
+    <div>
+      <Row title="General">
+        <StoryItem title="Basic">
+          <Select options={ ['one', 'two',' three'] } />
+        </StoryItem>
+        <StoryItem title="Placeholder">
+          <Select placeholder="choose" options={ ['one', 'two',' three'] } />
+        </StoryItem>
+        <StoryItem title="Invalid">
+          <Select options={ ['one', 'two',' three'] } valid={false} />
+        </StoryItem>
+      </Row>
+      <Row title="Options handling">
+        <StoryItem title="Options as children">
+          <Select>
+            <Option>one</Option>
+            <Option>two</Option>
+            <Option>three</Option>
+          </Select>
+        </StoryItem>
+        <StoryItem title="Options as property">
+          <Select options={ ['one', 'two',' three'] } />
+        </StoryItem>
+      </Row>
+      <Row title="Playground">
+        <StoryItem>
+          <Select
+            options={text('Options', 'one, two, three').split(/\s*,\s*/)}
+            placeholder={text('Placeholder', 'choose')}
+            defaultValue=''
+            valid={boolean('Valid', true)}
+            onChange={action('changed')}
+          />
+        </StoryItem>
+      </Row>
+    </div>
+  ));
