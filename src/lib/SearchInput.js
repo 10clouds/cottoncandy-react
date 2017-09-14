970697959ad5f9fe4e7e9ad00f46e2962aa4f110
Wiktor Latanowicz <wiktor.latanowicz@10clouds.com>
2017-09-14 16:21:12 +0200

    Button for non-expandable search input

--- src/lib/SearchInput.js
+++ src/lib/SearchInput.js
@@ -5,12 +5,21 @@ import styled from 'styled-components';
 import {palette, style} from '../constants';
 
 
-const searchIcon = `<svg xmlns='http://www.w3.org/2000/svg' width='1000' height='1000'>
-<path fill='${encodeURI(palette.TYPOGRAPHY.main)}' d='M963.1,833.6L704.8,575.3c0,0,0,0-6.1,0c30.7-61.5,49.2-123,
-49.2-190.6c0-202.9-166-368.9-368.9-368.9S10,181.8,10,384.7c0,202.9,166,368.9,368.9,368.9c67.6,0,135.3-18.4,190.6-55.3l0,
-0l258.2,258.2c36.9,36.9,92.2,36.9,129.1,0C999.9,925.8,999.9,870.5,963.1,833.6z M133,384.7c0-135.3,110.7-245.9,
-245.9-245.9c135.3,0,245.9,110.7,245.9,245.9c0,135.3-110.7,246-245.9,246C243.7,630.7,133,520,133,384.7z'/>
-</svg>`;
+const searchIcon = `<svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <g fill="${encodeURI(palette.TYPOGRAPHY.main)}">
+    <path d="M15.5608163,13.5026804 L11.3436735,9.24206186 C11.3436735,9.24206186 11.3436735,9.24206186
+      11.2440816,9.24206186 C11.7453061,8.22762887 12.0473469,7.21319588 12.0473469,6.09814433
+      C12.0473469,2.75134021 9.33714286,0.0131958763 6.0244898,0.0131958763 C2.71183673,0.0131958763
+      0,2.75134021 0,6.09814433 C0,9.44494845 2.71020408,12.1830928 6.02285714,12.1830928 C7.12653061,12.1830928
+      8.23183673,11.8795876 9.13469388,11.2709278 L9.13469388,11.2709278 L13.3502041,15.5298969
+      C13.9526531,16.1385567 14.8555102,16.1385567 15.4579592,15.5298969 C16.1616327,15.0235052
+      16.1616327,14.1113402 15.5608163,13.5026804 Z M2.00816327,6.09814433 C2.00816327,3.86639175
+      3.8155102,2.04206186 6.02285714,2.04206186 C8.23183673,2.04206186 10.037551,3.86804124 10.037551,6.09814433
+      C10.037551,8.32989691 8.23020408,10.1558763 6.02285714,10.1558763 C3.8155102,10.1558763
+      2.00816327,8.32989691 2.00816327,6.09814433 Z" id="Shape"></path>
+  </g>
+</svg>
+`;
 
 const StyledInput = styled.input`
   border: none;
@@ -19,17 +28,13 @@ const StyledInput = styled.input`
   height: 32px;
   font-size: 13px;
   color: ${palette.TYPOGRAPHY.main};
-  
-  background-color: ${style.BACKGROUND.main};
-  background-image: url("data:image/svg+xml;utf8,${searchIcon}");
-  background-size: 16px 16px;
-  background-repeat: no-repeat;
-  background-position: calc(100% - 10px) center;
-  
+  background: ${style.BACKGROUND.main};
+  padding-right: 32px;
+
   :focus {
     outline: 0;
     
-    & + div {
+    & + button + div {
       display: block;
     }
   }
@@ -37,6 +42,41 @@ const StyledInput = styled.input`
   ::placeholder {
     color: ${palette.SECONDARY.dark};
   }
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  right: 0;
+  border: none;
+  border-bottom: 1px solid ${palette.SECONDARY.base};
+  box-sizing: border-box;
+  height: 32px;
+  width: 32px;
+  color: ${palette.TYPOGRAPHY.main};
+  background: ${style.BACKGROUND.main};
+  cursor: pointer;
+  
+  :focus {
+    outline: 0;
+    > svg {
+      filter: drop-shadow( 0 0 2pt ${palette.PRIMARY.light} );
+    }
+  }
+  
+  > svg {
+    margin-top: 4px;
+  }
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const ShadowBox = styled.div`
+  box-shadow: ${style.SHADOW.outline};
+  margin: 0 5px;
+  display: none;
+  height: 0;
 `;
 
 const ExpandableStyledInput = styled.input`
@@ -53,7 +93,7 @@ const ExpandableStyledInput = styled.input`
   font-size: 13px;
   cursor: pointer;
   
-  background-image: url("data:image/svg+xml;utf8,${searchIcon}");
+  background-image: url('data:image/svg+xml;utf8,${searchIcon}');
   background-size: 16px 16px;
   background-repeat: no-repeat;
   background-position: center center;
@@ -81,22 +121,20 @@ const ExpandableStyledInput = styled.input`
   }
 `;
 
-const ShadowBox = styled.div`
-  box-shadow: ${style.SHADOW.outline};
-  margin: 0 5px;
-  display: none;
-  height: 0;
-`;
-
-const SearchInput = (props) =>
-  props.expandable ? (
-    <ExpandableStyledInput {...props} />
-  ) : (
-  <div>
-    <StyledInput {...props} />
-    <ShadowBox/>
-  </div>
-);
+const SearchInput = (props) => {
+  const{expandable, onClick, ...rest} = props;
+  return (
+    expandable ? (
+      <ExpandableStyledInput {...rest} />
+    ) : (
+      <Container>
+        <StyledInput {...rest} />
+        <StyledButton onClick={onClick} dangerouslySetInnerHTML={{__html: searchIcon}} />
+        <ShadowBox/>
+      </Container>
+    )
+  );
+};
 
 SearchInput.propTypes = {
   expandable: PropTypes.bool,
