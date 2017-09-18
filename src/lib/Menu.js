bc839eda056112c60bd57ba4a91577570f1fa1ea
Wiktor Latanowicz <wiktor.latanowicz@10clouds.com>
2017-09-18 10:10:19 +0200

    Two more hover styles for Menu

--- src/lib/Menu.js
+++ src/lib/Menu.js
@@ -14,7 +14,7 @@ const NavMenu = styled.ul`
 
 const NavItem = styled.li`
   display: inline-block;
-  padding: 16px 20px 24px;
+  padding: 16px 20px ${(p) => (!p.hasSubMenu && p.hoverLine === 'bottom') ? '0px' : '24px'};
   margin-top: 8px;
   
   & > ul {
@@ -30,7 +30,10 @@ const NavItem = styled.li`
 `;
 
 const SubbedNavItem = NavItem.extend`
-  :before {
+  padding-right: 24px;
+  white-space: nowrap;
+  
+  ::before {
     content: ' ';
     height: 6px;
     width: 6px;
@@ -38,7 +41,7 @@ const SubbedNavItem = NavItem.extend`
     border-right: 2px solid ${palette.TYPOGRAPHY.main};
     position: relative;
     top: 5px;
-    left: 5px;
+    left: 12px;
     float: right;
     transform: scale(0.70) rotate(45deg);
     transition: ${style.TRANSITION.main};
@@ -86,19 +89,31 @@ const SubNavItem = styled.li`
 `;
 
 const Nav = styled.header`
+  height: 69px;
   background-color: ${style.BACKGROUND.main};
   box-shadow: ${style.SHADOW.big};
   border-radius: 5px;
   padding: 0px 40px;
 `;
 
+const MenuNavLink = NavLink.extend`
+  background: ${style.BACKGROUND.main};
+  display: ${(p) => (!p.hasSubMenu && !p.isSubMenu && p.hoverLine === 'bottom') ? 'block' : 'inline-block' };
+  height: ${(p) => (!p.hasSubMenu && !p.isSubMenu && p.hoverLine === 'bottom') ? '40px' : 'auto'};
+  border-bottom-style: ${(p) => (p.hasSubMenu && p.hoverLine === 'bottom') || p.hoverLine === 'none' ? 'none' : 'solid' };
+`;
+
 const Submenu = (props) => (
   <NavMenu>
     { props.links.map((item, index) =>
       <SubNavItem key={item.name + index}>
-        <NavLink href={item.link}>
+        <MenuNavLink
+          href={item.link}
+          hoverLine={props.hoverLine}
+          isSubMenu
+        >
           {item.name}
-        </NavLink>
+        </MenuNavLink>
       </SubNavItem>
     )}
   </NavMenu>
@@ -109,17 +124,25 @@ const Menu = (props) => (
     <NavMenu>
       {props.links.map((item, index) =>
         item.submenu ? (
-            <SubbedNavItem key={item.name + index}>
-              <NavLink href={item.link}>
+            <SubbedNavItem
+              key={item.name + index}
+              hoverLine={props.hoverLine}
+              hasSubMenu
+            >
+              <MenuNavLink
+                href={item.link}
+                hoverLine={props.hoverLine}
+                hasSubMenu
+              >
                 {item.name}
-              </NavLink>
-              <Submenu links={item.submenu} />
+              </MenuNavLink>
+              <Submenu links={item.submenu} hoverLine={props.hoverLine}/>
             </SubbedNavItem>
           ) : (
-            <NavItem key={item.name + index}>
-              <NavLink href={item.link}>
+            <NavItem key={item.name + index} hoverLine={props.hoverLine}>
+              <MenuNavLink href={item.link} hoverLine={props.hoverLine}>
                 {item.name}
-              </NavLink>
+              </MenuNavLink>
             </NavItem>
           )
       )}
@@ -128,6 +151,7 @@ const Menu = (props) => (
 );
 
 Menu.propTypes = {
+  hoverLine: PropTypes.oneOf(['none', 'underline', 'bottom']),
   links: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
@@ -136,6 +160,10 @@ Menu.propTypes = {
       link: PropTypes.string.isRequired,
     })),
   })).isRequired,
+};
+
+Menu.defaultProps = {
+  hoverLine: 'underline',
 };
 
 export default Menu;
--- src/stories/Menu.story.js
+++ src/stories/Menu.story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 
 import { Row, StoryItem } from './utils.js';
 
@@ -68,6 +68,55 @@ stories
               name: "Item 4"
             }
           ]}/>
+        </StoryItem>
+      </Row>
+      <Row title="Playground">
+        <StoryItem>
+          <Menu
+            hoverLine={select('Hover line style', ['none', 'underline', 'bottom'], 'underline')}
+            links={JSON.parse(
+              text(
+                'Links',
+                JSON.stringify(
+                  [
+                    {
+                      link: 'http://10clouds.com/',
+                      name: 'Item 1'
+                    },
+                    {
+                      link: 'http://10clouds.com/',
+                      name: 'Item 2'
+                    },
+                    {
+                      link: 'http://10clouds.com/',
+                      name: 'Item 3',
+                      submenu: [
+                        {
+                          link: 'http://10clouds.com/',
+                          name: 'Subitem 1',
+                        },
+                        {
+                          link: 'http://10clouds.com/',
+                          name: 'Subitem 2',
+                        },
+                        {
+                          link: 'http://10clouds.com/',
+                          name: 'Subitem 3',
+                        }
+                      ]
+                    },
+                    {
+                      link: 'http://10clouds.com/',
+                      name: 'Item 4',
+                    }
+                  ],
+                  null,
+                  2
+                )
+              )
+            )}
+          />
+          <div style={{height: '200px'}}></div>
         </StoryItem>
       </Row>
     </div>
